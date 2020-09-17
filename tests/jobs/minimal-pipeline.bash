a72dc5204fa2e7f3eb01bc4f48d7363061802fc8
Aaron Fishman <uk.afishman@gmail.com>
2020-09-17 09:37:41 +0100

    test script as circleci replacement

---
+++ tests/jobs/minimal-pipeline.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set paths
+cd /BovTB-nf/
+mkdir /results/
+ln -s $PWD/tests/minimal-read-pair/ /reads
+
+# Run nextflow
+nextflow run bTB-WGS_process.nf --outdir "/results/" --reads "/reads/*_{S*_R1,S*_R2}*.fastq.gz"
+
+# Check results
+WGS_CLUSTER_CSV=/results/`sh tests/utils/print_todays_wgs_cluster.sh`
+python tests/utils/assert_first_csv_row.py $WGS_CLUSTER_CSV Outcome InsufficientData
+
