4ae644ecd5ef2da6cb11411b24353dc22965679c
Aaron Fishman <uk.afishman@gmail.com>
2020-09-22 10:58:56 +0100

    testing for B6-16

--- tests/jobs/inclusivity.bash
+++ tests/jobs/inclusivity.bash
@@ -17,10 +17,11 @@ ln -s $PWD/00090/* /reads
 # Run nextflow
 nextflow run bTB-WGS_process.nf \
 --outdir "/results/" \
---reads "/reads/*_{S*_R1,S*_R2}*.fastq.gz" \
+--reas "/reads/*_{S*_R1,S*_R2}*.fastq.gz" \
 --lowmem '"--memory-map"' \
 -with-report "/results/report.html"
 
 # Check results
 WGS_CLUSTER_CSV=/results/`sh tests/utils/print_todays_wgs_cluster.sh`
-python tests/utils/assert_first_csv_row.py $WGS_CLUSTER_CSV Outcome InsufficientData
+python tests/utils/assert_first_csv_row.py $WGS_CLUSTER_CSV Outcome Pass
+python tests/utils/assert_first_csv_row.py $WGS_CLUSTER_CSV group B6-16
