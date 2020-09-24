ceace9b27998fa71e9117e3cbba953d85df491c5
Aaron Fishman <uk.afishman@gmail.com>
2020-09-24 15:09:29 +0100

    fixing minimal pipeline

--- tests/jobs/minimal-pipeline.bash
+++ tests/jobs/minimal-pipeline.bash
@@ -10,16 +10,16 @@
 
 
 # Set paths
-cd /BovTB-nf/
-mkdir /results/
-ln -s $PWD/tests/data/minimal-read-pair/ /reads
+mkdir -p /results/
+mkdir -p /reads/MRP/
+cp -r $PWD/tests/data/minimal-read-pair/* /reads/MRP/
 
 # Run nextflow
 nextflow run bTB-WGS_process.nf \
 --outdir "/results/" \
---reads "/reads/*_{S*_R1,S*_R2}*.fastq.gz" \
+--reads "/reads/MRP/*_{S*_R1,S*_R2}*.fastq.gz" \
 --lowmem '"--memory-map"' \
--with-report "/results/report.html"
+-with-report "/results/reports/report.html"
 
 # Check results
 WGS_CLUSTER_CSV=/results/`sh tests/utils/print_todays_wgs_cluster.sh MRP`
