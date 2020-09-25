55b663152eeb4f74a0fa649e68db349722b363b7
Aaron Fishman <uk.afishman@gmail.com>
2020-09-25 08:55:03 +0100

    tinyreads bug

--- tests/jobs/tinyreads.bash
+++ tests/jobs/tinyreads.bash
@@ -10,17 +10,23 @@
 
 
 # Set paths
-mkdir -p /results/
-mkdir -p /reads/tinyreads/
-cp -r $PWD/tests/data/tinyreads/* /reads/tinyreads/
+reads=/reads/tinyreads/
+results=/results/
+
+mkdir -p $results
+mkdir -p $reads
+
+# Copy Data
+cp -r $PWD/tests/data/tinyreads/* $reads
 
 # Run nextflow
 nextflow run bTB-WGS_process.nf \
---outdir "/results/" \
---reads "/reads/MRP/*_{S*_R1,S*_R2}*.fastq.gz" \
+--outdir "$results" \
+--reads "$reads/*_{S*_R1,S*_R2}*.fastq.gz" \
 --lowmem '"--memory-map"' \
 -with-report "/artifacts/report.html"
 
 # Check results
-WGS_CLUSTER_CSV=/results/`sh tests/utils/print_todays_wgs_cluster.sh tinyreads`
-python tests/utils/assert_first_csv_row.py $WGS_CLUSTER_CSV Outcome InsufficientData
+WGS_CLUSTER_CSV=${results}/`sh tests/utils/print_todays_wgs_cluster.sh tinyreads`
+python tests/utils/assert_first_csv_row.py $WGS_CLUSTER_CSV \
+    Outcome InsufficientData
