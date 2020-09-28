080160e005a0daafa38392ca8c25effa685268bd
Aaron Fishman <uk.afishman@gmail.com>
2020-09-28 15:21:01 +0100

    nextflow test

--- tests/utils/nextflowtest.bash
+++ tests/utils/nextflowtest.bash
@@ -1,3 +1,12 @@
+#!/bin/bash
+#
+#================================================================
+# nextflowtest.bash
+#================================================================
+#
+#% DESCRIPTION
+#%    Integration tests for nextflow should be called this way. 
+
 nextflow run bTB-WGS_process.nf \
 --outdir "/results/" \
 --reads "/reads/*_{S*_R1,S*_R2}*.fastq.gz" \
