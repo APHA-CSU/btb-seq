ccb8a9f3173511668c93643d4a05a4128b75688f
Aaron Fishman <uk.afishman@gmail.com>
2021-01-13 12:20:45 +0000

    dropping depend_path for mask.bash

--- bin/mask.bash
+++ bin/mask.bash
@@ -4,7 +4,7 @@
 pair_id=$1
 rptmask=$2
 
-$dependpath/bedtools2/bin/bedtools genomecov -bga -ibam ${pair_id}.mapped.sorted.bam |
+bedtools genomecov -bga -ibam ${pair_id}.mapped.sorted.bam |
 grep -w "0\$" | cat > ${pair_id}_zerocov.bed
 cat ${pair_id}_zerocov.bed $rptmask | sort -k1,1 -k2,2n |
-$dependpath/bedtools2/bin/bedtools merge > ${pair_id}_RptZeroMask.bed
+bedtools merge > ${pair_id}_RptZeroMask.bed
