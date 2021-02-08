8e92a658c9cb439d20463685c255e5a72bdb6be6
ellisrichardj <richard.ellis@apha.gov.uk>
2021-02-08 17:48:12 +0000

    Change hashbang to more portable version

--- bin/CombineCsv.py
+++ bin/CombineCsv.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 
 # Merge the csv output files and determine whether samples are M.bovis positive or not, based on predetemined 
 # thresholds for number and proportion of M.bovis reads.
