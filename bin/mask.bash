2a5597f62a49ed9de7f36b4fcfe464c97390cf7a
Aaron Fishman <uk.afishman@gmail.com>
2021-02-10 11:00:28 +0000

    removing setup and teardown routines

--- bin/mask.bash
+++ bin/mask.bash
@@ -5,6 +5,6 @@ pair_id=$1
 rptmask=$2
 
 bedtools genomecov -bga -ibam ${pair_id}.mapped.sorted.bam |
-grep -w "0\$"  > ${pair_id}_zerocov.bed
+grep -w "0\$" | cat > ${pair_id}_zerocov.bed
 cat ${pair_id}_zerocov.bed $rptmask | sort -k1,1 -k2,2n |
 bedtools merge > ${pair_id}_RptZeroMask.bed
