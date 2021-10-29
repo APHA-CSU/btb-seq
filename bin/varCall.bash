86b8ca1bf9929631e5a42015bd9e054bd0f76c95
Eleftheria Palkopoulou <elle.palkopoulou@gmail.com>
2021-10-29 20:19:35 +0100

    default base quality filter

--- bin/varCall.bash
+++ bin/varCall.bash
@@ -21,6 +21,6 @@ bam=$2
 vcf=$3
 
 samtools index $bam
-bcftools mpileup -Q 10 -Ou -f $ref "$bam" |
+bcftools mpileup -Ou -f $ref "$bam" |
 bcftools call -mf GQ - -Ou |
 bcftools norm -f $ref - -Oz -o "$vcf"
