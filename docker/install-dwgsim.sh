738cb17bd8dc6c70087cf824e402b4cee77d39e5
Aaron Fishman <uk.afishman@gmail.com>
2021-11-01 15:29:42 +0000

    using https to install dwgsim

--- docker/install-dwgsim.sh
+++ docker/install-dwgsim.sh
@@ -1,6 +1,6 @@
 # dwgsim
 
-git clone --recursive git@github.com:nh13/DWGSIM.git
+git clone --recursive https://github.com/nh13/DWGSIM.git
 cd DWGSIM/
 make
 sudo ln -s $PWD/dwgsim /usr/local/bin/dwgsim
