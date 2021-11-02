2c5b1b08391e7a156a1676ee3847591fe6033fbf
Eleftheria Palkopoulou <elle.palkopoulou@gmail.com>
2021-11-02 11:35:09 +0000

    removes SNPs that are within 10bp from each other

--- bin/vcf2Consensus.bash
+++ bin/vcf2Consensus.bash
@@ -28,27 +28,32 @@ snps=$5
 VAR_QUAL=$6
 MIN_READ_DEPTH=$7
 MIN_ALT_PROPORTION=$8
+window=$9
 
 # Filter
 bcf=filtered.bcf
+masked_bcf=masked.bcf
 filt_vcf=filtered.vcf
 bcftools filter -e 'TYPE!="snp" || %QUAL<${VAR_QUAL} || (AD[0]+AD[1])<=${MIN_READ_DEPTH} || ADF[1]==0 || ADR[1]==0 || (AD[1]/(AD[0]+AD[1]))<=${MIN_ALT_PROPORTION} ' $vcf -Ob -o $bcf
-bcftools index $bcf
-bcftools view $bcf -Oz -o $filt_vcf
+
+vcftools --bcf $bcf --thin $window --recode-bcf --recode-INFO-all --out $masked_bcf
+
+bcftools index $masked_bcf
+bcftools view $masked_bcf -Oz -o $filt_vcf
 
 # Call Consensus
 base_name=`basename $consensus`
 name="${base_name%%.*}"
 
-bcftools consensus -f ${ref} -e 'TYPE="indel"' -m $bed $bcf |
+bcftools consensus -f ${ref} -e 'TYPE="indel"' -m $bed $masked_bcf |
 sed "/^>/ s/.*/>${name}/" > $consensus
 
 # Write SNPs table
 echo -e 'CHROM\tPOS\tTYPE\tREF\tALT\tEVIDENCE' > $snps
 
-bcftools query -e 'TYPE="REF"' -f '%CHROM,%POS,%TYPE,%REF,%ALT,%DP4\n' $bcf |
+bcftools query -e 'TYPE="REF"' -f '%CHROM,%POS,%TYPE,%REF,%ALT,%DP4\n' $masked_bcf |
 awk -F, '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$5":"$8+$9" "$4":"$6+$7}' >> $snps
 
 # Cleanup
-rm $bcf $bcf.csi
+rm $bcf $masked_bcf $bcf.csi
 
