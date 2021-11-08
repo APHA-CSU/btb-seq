ec7c309e5e76ad43a318ca901e05a618c2276914
Aaron Fishman <uk.afishman@gmail.com>
2021-11-08 11:15:35 +0000

    cleaning up ofat code

--- sim/ofat.py
+++ sim/ofat.py
@@ -1,14 +1,13 @@
 import validator
 import os
+import argparse
 
-#
-results_root = '/home/aaronfishman/ebs/pipeline-results/ofat-4/'
-results_root = os.path.join(results_root, '')
-repo_path = '/home/aaronfishman/temp2/btb-seq'
-reference_path = '/home/aaronfishman/repos/btb-seq/sim/Mycobacterium_bovis_AF212297_LT78304.fa'
+"""
+    Run the performance benchmarking tool against a number of git branches
+"""
 
-# First download them
-branches = [
+# Initial list of branches that we are testing
+DEFAULT_BRANCHES = [
     "BaseQual",
     "MapQual",
     "Ploidy",
@@ -20,16 +19,31 @@ branches = [
     "RepeatMask"
 ]
 
-def main():
+def ofat(btb_seq_path, results_path, branches=DEFAULT_BRANCHES):
+    """ Runs a performance test against the pipeline
 
-    os.makedirs(results_root, exist_ok=False)
+        Parameters:
+            btb_seq_path (str): Path to btb-seq code is stored
+            results_path (str): Output path to performance test results
+            branches (list): List of strings for each git branch to test
+    """
+    # Add trailing slash
+    repo_path = os.path.join(repo_path, '')
+    results_path = os.path.join(results_path, '')     
+    
+    # Default reference for sims
+    reference_path = repo_path + './sim/Mycobacterium_bovis_AF212297_LT78304.fa'
 
+    # Prepare output directory
+    os.makedirs(results_path, exist_ok=False)
+
+    # Run against branches
     for branch in branches:
-        results_path = results_root + branch + '/'
+        branch_results_path = results_path + branch + '/'
 
         try:
             validator.performance_test(
-                results_path, 
+                branch_results_path, 
                 repo_path, 
                 reference_path, 
                 branch=branch
@@ -38,4 +52,13 @@ def main():
             print(f"***FAILED BRANCH: {branch}****", branch)
 
 if __name__ == '__main__':
-    main()
+    # Parse
+    parser = argparse.ArgumentParser(description="Run the performance benchmarking tool against a number of git branches")
+
+    parser.add_argument("results", help="path to results directory")
+    parser.add_argument("btb_seq", help="path to btb-seq code")
+
+    args = parser.parse_args(sys.argv[1:])
+
+    # Run
+    ofat(args.results, args.btb_seq)
