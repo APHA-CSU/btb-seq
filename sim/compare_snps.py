7b7a5a5a58a2c003bc6151e3209546b6dd0092a9
Nick Pestell <nicholas.pestell@apha.gov.uk>
2021-11-09 12:02:08 +0000

    details file

--- sim/compare_snps.py
+++ sim/compare_snps.py
@@ -1,6 +1,7 @@
 import pandas as pd
-
+import subprocess
 from Bio import SeqIO
+from io import StringIO
 
 """
 Calculate performance stats from simulated data
@@ -41,53 +42,95 @@ def analyse(simulated_snps, pipeline_snps):
     pipeline_pos_adjusted = pipeline_pos - masked_pos
 
     # TP - true positive -(the variant is in the simulated genome and correctly called by the pipeline)
-    tp = len(simulated_pos.intersection(pipeline_pos_adjusted))
+    tp = simulated_pos.intersection(pipeline_pos_adjusted)
+    tp_rate = len(tp)
 
     # FP (the pipeline calls a variant that is not in the simulated genome),
-    fp = len(pipeline_pos_adjusted - simulated_pos_adjusted)
+    fp = pipeline_pos_adjusted - simulated_pos_adjusted
+    fp_rate = len(fp)
 
     # FN SNP calls (the variant is in the simulated genome but the pipeline does not call it).
-    fn =  len(simulated_pos_adjusted - pipeline_pos_adjusted)
+    fn = simulated_pos_adjusted - pipeline_pos_adjusted
+    fn_rate =  len(fn)
 
     # TPs excluded 
-    masked_tp = len(masked_pos.intersection(simulated_pos.intersection(pipeline_pos)))
+    masked_tp = masked_pos.intersection(simulated_pos.intersection(pipeline_pos))
+    masked_tp_rate = len(masked_tp)
 
     # FPs excluded
-    masked_fp = len(masked_pos.intersection(simulated_pos - pipeline_pos))
+    masked_fp = masked_pos.intersection(simulated_pos - pipeline_pos)
+    masked_fp_rate = len(masked_fp)
 
     # FNs excluded
-    masked_fn = len(masked_pos.intersection(simulated_pos - pipeline_pos))
+    masked_fn = masked_pos.intersection(simulated_pos - pipeline_pos)
+    masked_fn_rate = len(masked_fn)
 
     # Compute Performance Stats
     # precision (positive predictive value) of each pipeline as TP/(TP + FP), 
-    precision = tp / (tp + fp)
+    precision = tp_rate / (tp_rate + fp_rate)
 
     # recall (sensitivity) as TP/(TP + FN)
-    sensitivity = tp / (tp + fn)
+    sensitivity = tp_rate / (tp_rate + fn_rate)
 
     # miss rate as FN/(TP + FN)
-    miss_rate = fn / (tp + fn)
+    miss_rate = fn_rate / (tp_rate + fn_rate)
 
     #  total number of errors (FP + FN) per million sequenced bases
-    total_errors = fp + fn
+    total_errors = fp_rate + fn_rate
+
+    make_details_file('/home/nickpestell/pipeline-results/btb-seq-9/simulated-genome','/home/nickpestell/pipeline-results/btb-seq-9/btb-seq-results/Results_simulated-reads_09Nov21', fn)
 
     return {
-        "TP": tp,
-        "FP": fp,
-        "FN": fn,
-        "masked TPs": masked_tp,
-        "masked FPs": masked_fp,
-        "masked FNs": masked_fn,
+        "TP": tp_rate,
+        "FP": fp_rate,
+        "FN": fn_rate,
+        "masked TPs": masked_tp_rate,
+        "masked FPs": masked_fp_rate,
+        "masked FNs": masked_fn_rate,
         "precision": precision,
         "sensitivity": sensitivity,
         "miss_rate": miss_rate,
         "total_errors": total_errors
     }
 
+def make_details_file(sim_path, pl_path, sites):
+    simulated_genome = load_consensus(sim_path+'/simulated.simseq.genome.fa')
+    pipeline_genome = load_consensus(pl_path+'/consensus/simulated.fas')
+    pipeline_vcf = load_vcf(pl_path+'/vcf/simulated.vcf.gz')
+    with open(pl_path+'/details.txt','w') as details_file:
+        for i in sites:
+            details_file.write("##POSITION: {}".format(i)+'\n')
+            details_file.write('##SIMULATED GENOME:\n')
+            details_file.write(simulated_genome[i-51:i-1].lower()+simulated_genome[i-1]+simulated_genome[i:i+50].lower()+'\n')
+            details_file.write('##PIPELINE GENOME:\n')
+            details_file.write(pipeline_genome[i-49:i+1].lower()+pipeline_genome[i+1]+pipeline_genome[i+2:i+52].lower()+'\n')
+            details_file.write("#PIPELINE VCF:\n")
+            details_file.write(pipeline_vcf.loc[pipeline_vcf['POS'] == i].to_csv(sep='\t')+'\n')
+            details_file.write("\n\n")
+
+
+    x = 0
+
 #TODO: This may not be required if we can get away with using bcftools/vcftools
 #      for comparisons. Leaving this here for convenience in case those tools aren't suitable 
 def load_consensus(path):
     """ Load a consensus file. Returns the first record in a fasta file a string """
 
-    for seq_record in SeqIO.parse(path, "fasta"):
-        return str(seq_record)
+    seq_record = SeqIO.read(path, "fasta")
+    return str(seq_record.seq)
+
+def load_vcf(path):
+    returncode = subprocess.run(['gunzip',
+                                 '-k',
+                                 path]).returncode
+    if returncode:
+        raise Exception("""*****
+            %s
+            cmd failed with exit code %i
+          *****""" % ('gunzip', returncode))
+    with open(path[:-3], 'r') as vcf_file:
+        vcf_data = [l for l in vcf_file if not l.startswith('##')]
+    vcf_df = pd.read_csv(StringIO(''.join(vcf_data)), delimiter = '\t')
+
+    return vcf_df
+    
