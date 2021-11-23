1cede084d5180d48681b809a64972a48d224b4f8
Eleftheria Palkopoulou <elle.palkopoulou@gmail.com>
2021-11-23 21:11:02 +0000

    remove repeat mask

--- bin/vcf2Consensus.bash
+++ bin/vcf2Consensus.bash
@@ -29,10 +29,10 @@ MIN_READ_DEPTH=$6
 
 # Filter
 bcf=filtered.bcf
-filt_vcf=filtered.vcf
+#filt_vcf=filtered.vcf
 bcftools filter -e "TYPE!='snp' || (AD[0]+AD[1])<=${MIN_READ_DEPTH}" $vcf -Ob -o $bcf
 bcftools index $bcf
-bcftools view $bcf -Oz -o $filt_vcf 
+#bcftools view $bcf -Oz -o $filt_vcf 
 
 # Call Consensus
 base_name=`basename $consensus`
@@ -48,4 +48,4 @@ bcftools query -e 'TYPE="REF"' -f '%CHROM,%POS,%TYPE,%REF,%ALT,%DP4\n' $bcf |
 awk -F, '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$5":"$8+$9" "$4":"$6+$7}' >> $snps
 
 # Cleanup
-rm $bcf $bcf.csi
+#rm $bcf $bcf.csi
