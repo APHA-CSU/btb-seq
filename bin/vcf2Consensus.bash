d43f5785d5f5422f3c752f6fc0d9fd95e08ae07c
Aaron Fishman <uk.afishman@gmail.com>
2022-01-31 17:44:13 +0000

    snps overwriting ref bug

--- bin/vcf2Consensus.bash
+++ bin/vcf2Consensus.bash
@@ -33,19 +33,26 @@ bcf=$6
 
 # Filter
 bcftools filter --IndelGap $INDEL_GAP -i \
-    "(DP>=${MIN_READ_DEPTH} && AD[1]>=${MIN_READ_DEPTH} && INFO/AD[1]/(INFO/AD[1]+INFO/AD[0]) >= ${MIN_ALLELE_FREQUENCY}) \
-    || (DP>=${MIN_READ_DEPTH} && AD[0]>=${MIN_READ_DEPTH} && INFO/AD[0]/(INFO/AD[1]+INFO/AD[0]) >= ${MIN_ALLELE_FREQUENCY}) "\
-    $vcf -Ob -o $bcf
-bcftools index $bcf
+    "(DP>=${MIN_READ_DEPTH} && AD[1]>=${MIN_READ_DEPTH} && INFO/AD[1]/(INFO/AD[1]+INFO/AD[0]) >= ${MIN_ALLELE_FREQUENCY})" \
+$vcf -Ob -o snps.bcf
+bcftools index snps.bcf
+bcftools view snps.bcf > snps.vcf
 
-# Bcf to vcf
-bcftools view $bcf > filtered.vcf
+bcftools filter --IndelGap $INDEL_GAP -i \
+    "(DP>=${MIN_READ_DEPTH} && AD[0]>=${MIN_READ_DEPTH} && INFO/AD[0]/(INFO/AD[1]+INFO/AD[0]) >= ${MIN_ALLELE_FREQUENCY})" \
+    $vcf -Ob -o refs.bcf
+bcftools index refs.bcf
+bcftools view refs.bcf > refs.vcf
 
-# Add unfiltered output to mask
+# Concatenate
+bcftools concat snps.vcf refs.vcf > filtered.vcf
+
+#TODO: Better names
 G=/home/aaronfishman/repos/btb-seq/references/full_genome.bed
 F=filtered.vcf
 M=$bed
 
+# Add unfiltered output to mask
 bedtools subtract -a $G -b $F > subtracted.bed
 
 # Add
@@ -53,15 +60,17 @@ cat subtracted.bed $M |
 sort -k1,1 -k2,2n |
 bedtools merge > consensus_mask.bed
 
+echo -C-
+
 # Call Consensus
 base_name=`basename $consensus`
 name="${base_name%%.*}"
 
-bcftools consensus -f ${ref} -e 'TYPE="indel"' -m consensus_mask.bed $bcf |
+bcftools consensus -f ${ref} -e 'TYPE="indel"' -m consensus_mask.bed snps.bcf |
 sed "/^>/ s/.*/>${name}/" > $consensus
 
 # Write SNPs table
 echo -e 'CHROM\tPOS\tTYPE\tREF\tALT\tEVIDENCE' > $snps
 
-bcftools query -e 'TYPE="REF"' -f '%CHROM,%POS,%TYPE,%REF,%ALT,%DP4\n' $bcf |
+bcftools query -e 'TYPE="REF"' -f '%CHROM,%POS,%TYPE,%REF,%ALT,%DP4\n' snps.vcf.gz |
 awk -F, '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$5":"$8+$9" "$4":"$6+$7}' >> $snps
