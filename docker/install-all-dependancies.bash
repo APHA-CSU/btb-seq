75c253a23b0a10943f9d4b5f1e35fce1a0fd7eb4
Nick Pestell <nick@njpestell.com>
2022-03-15 16:37:19 +0000

    install python

--- docker/install-all-dependancies.bash
+++ docker/install-all-dependancies.bash
@@ -20,5 +20,9 @@ mkdir -p $BIOTOOLS_PATH
 cp ./install*.*sh $BIOTOOLS_PATH
 cd $BIOTOOLS_PATH
 
+# python 
+pip3 install biopython numpy pandas gitpython
+ln -s /usr/bin/python3 /usr/bin/python
+
 bash -e install-Kraken2.sh
 bash -e install-bracken.sh
