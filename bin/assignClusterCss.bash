36355a7bcbeb7a3538ab2fbbc4ea674d350c67f5
Aaron Fishman <uk.afishman@gmail.com>
2022-04-22 09:05:31 +0100

    set -eo pipefail on assign cluster

--- bin/assignClusterCss.bash
+++ bin/assignClusterCss.bash
@@ -3,6 +3,9 @@
 # Assigns cluster by matching patterns of cluster specific SNPs
 # Compares SNPs identified in vcf file to lists in reference table
 
+# Error handling
+set -eo pipefail
+
 pair_id=$1
 discrimPos=$2
 stage1pat=$3
@@ -13,10 +16,6 @@ min_qual_snp=$7
 min_qual_nonsnp=$8
 pypath=$9
 ref=$10
-
-# Error handling
-set -e
-set pipefail
 
 bcftools view -R ${discrimPos} -O v -o ${pair_id}.discrimPos.vcf ${pair_id}.vcf.gz
 python3 $pypath/Stage1-test.py ${pair_id}_stats.csv ${stage1pat} $ref test ${min_mean_cov} ${min_cov_snp} ${alt_prop_snp} ${min_qual_snp} ${min_qual_nonsnp} ${pair_id}.discrimPos.vcf
