f136a57e151955614254830d2be2bd87202a639b
Priza0m <103438560+Priza0m@users.noreply.github.com>
2023-08-04 09:57:32 +0100

    Uploaded Bed_Merge.py

---
+++ pyscripts/Bed_Merge.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pandas as pd
+import sys
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+args=sys.argv
+
+if len(args)<2:
+    positions = "/home/prizamsandhu/mnt/fsx-036/Prizam/vcf_filtering_test/pos"
+    mask = "/home/prizamsandhu/mnt/fsx-036/Prizam/vcf_filtering_test/DataDrivenMerge20.bed"
+else:
+    mask = sys.argv[1]
+    positions =sys.argv[2]
+
+f = open(positions).read().split('\n')
+
+f = f[:-1]
+pos = []
+
+for x in f:
+    pos.append(int(x))
+
+distance = 10
+remove = []
+counter = 0
+for x in pos:
+    if counter == 0:
+        x_upper = x + distance
+        if pos[(counter+1)] < x_upper:
+            remove.append(x)
+    elif counter == (len(pos) - 1):
+        x_lower = x - distance
+        if pos[(counter-1)] > x_lower:
+            remove.append(x)
+    else:
+        x_upper = x + distance
+        x_lower = x - distance
+        if pos[(counter+1)] < x_upper or pos[(counter-1)] > x_lower:
+            remove.append(x)
+    counter = counter + 1
+
+counter = 0
+chain = []
+for x in remove:
+    if counter == 0:
+        start = x
+    x_upper = x + distance
+    if counter == (len(remove) - 1):
+        end = x
+        chain.append([start,end])
+        break
+    if remove[(counter + 1)] > x_upper:
+        end = x
+        chain.append([start,end])
+        start = remove[(counter + 1)]
+    counter = counter + 1
+
+bedfile = pd.DataFrame(columns=["Sample","Start","End"])
+for x in chain:
+    bedfile.loc[-1] = ["LT708304-Mycobacteriumbovis-AF2122-97",(x[0]-1),x[1]]
+    bedfile.index = bedfile.index + 1
+    bedfile = bedfile.sort_index()
+    
+f = open(mask).read().split('\n')
+f = f[:-1]
+datadriven = []
+for x in f:
+    datadriven.append(x.split("\t"))
+    
+for x in datadriven:
+    bedfile.loc[-1] = ["LT708304-Mycobacteriumbovis-AF2122-97",int(x[1]),int(x[2])]
+    bedfile.index = bedfile.index + 1
+    bedfile = bedfile.sort_index()
+
+bedfile = bedfile.sort_values("Start")
+
+bedfile.to_csv('mask.bed', sep='\t', index=False, header=None)
+
+print(dname+"/mask.bed")
