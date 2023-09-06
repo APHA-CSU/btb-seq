ec7bf4a165927c8009e0a90e9500f1cd29b90468
ellisrichardj <richard.ellis@apha.gov.uk>
2023-09-06 16:57:09 +0100

    include threshold for minimum clean reads

--- bin/readStats.bash
+++ bin/readStats.bash
@@ -48,12 +48,12 @@ pair_id=$1
     minpc=60 # require at least 60% of data maps to genome
     minreads=600000 # require at least 600,000 raw reads per sample
     minafttrim=60 # require at least 60% reads to pass quality filtering steps
-
+    minclean=90 # require at least 90% of reads to be M.bovis
 
 # This section assigns 'flags' based on the number of reads and the proportion mapping to reference genome
     
     if [[ ${pc_aft_trim%%.*} -lt $minafttrim ]]; then flag="LowQualData"
-        elif [[ ${avg_depth%%.*} -ge $mindepth ]] && [[ ${pc_mapped%%.*} -gt $minpc ]]; then flag="Pass"
+        elif [[ ${avg_depth%%.*} -ge $mindepth ]] && [[ ${pc_mapped%%.*} -gt $minpc ]] && [[ ${pc_clean%%.*} -gt %minclean ]]; then flag="Pass"
         elif [[ ${avg_depth%%.*} -lt $mindepth ]] && [[ ${pc_mapped%%.*} -lt $minpc ]] && [[ $num_trim -gt $minreads ]]; then flag="Contaminated"
         elif [[ ${avg_depth%%.*} -lt $mindepth ]] && [[ $num_trim -lt $minreads ]]; then flag="InsufficientData"
 #        elif [ ${pc_mapped%%.*} -lt $minpc ] && [ $num_trim -gt $minreads ]; then flag="q_OtherMycobact"
