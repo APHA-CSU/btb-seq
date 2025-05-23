7f24dc340cd3ed4426671e89a0935d72486d6307
ellisrichardj <richard.ellis@apha.gov.uk>
2025-05-23 15:22:08 +0100

    remove individual install scripts

--- docker/install-bwa.sh
+++
@@ -1,7 +0,0 @@
-# bwa
-git clone https://github.com/lh3/bwa.git
-cd bwa
-git checkout v0.7.17
-make
-cd ..
-sudo ln -s $PWD/bwa/bwa /usr/local/bin/bwa
--- docker/install-nextflow.sh
+++
@@ -1,3 +0,0 @@
-# Install nextflow.
-cat ./install-nextflow-22.10.8.bash | bash
-sudo ln -s $PWD/nextflow /usr/local/bin/nextflow
