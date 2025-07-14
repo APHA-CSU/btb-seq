e4bf66ee9ff295e3cd084e29db99dead3f9037ff
ellisrichardj <richard.ellis@apha.gov.uk>
2025-07-14 16:20:21 +0100

    update scoring thresholds

--- bin/assignClade.py
+++ bin/assignClade.py
@@ -63,10 +63,10 @@ def assign(samplefas, CSStable):
     sortedScore_df = data.sort_values('Score', ascending=False).head(10)
     
     
-    sortedScore_df.loc[(sortedScore_df['Score'] > 2800), 'flag'] = 'Match'
-    sortedScore_df.loc[(sortedScore_df['Score'] > 2500) & (sortedScore_df['Score'] <= 2800),
+    sortedScore_df.loc[(sortedScore_df['Score'] > 3200), 'flag'] = 'Match'
+    sortedScore_df.loc[(sortedScore_df['Score'] > 2800) & (sortedScore_df['Score'] <= 3200),
                        'flag'] = 'Tentative'
-    sortedScore_df.loc[(sortedScore_df['Score'] <= 2500), 'flag'] = 'Outlier'
+    sortedScore_df.loc[(sortedScore_df['Score'] <= 2800), 'flag'] = 'Outlier'
     
     #sortedScore_df.loc[(sortedScore_df['flag'] == 'Outlier'), 'group'] = 'NoMatch'
     print(sortedScore_df)
