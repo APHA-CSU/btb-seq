3ae56ecd8e3afcf135e8049e808da28ec14e41cd
ellisrichardj <richard.ellis@apha.gov.uk>
2025-08-05 22:48:12 +0100

    fix if evaluation

--- bin/readStats.bash
+++ bin/readStats.bash
@@ -25,8 +25,8 @@ rm_inter=$2
     zero_cov=$(awk 'BEGIN {count=0} $3<1 {++count} END {print count}' depth.txt)
     sites=$(awk '{++count} END {print count}' depth.txt)
     rm depth.txt
-    if $rm_inter = 'true';
-        then rm `readlink ${pair_id}_uniq_R2.fastq`
+    if [ $rm_inter = 'true' ]; then
+        rm `readlink ${pair_id}_uniq_R2.fastq`
     fi
     
 
